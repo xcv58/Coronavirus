dd5129a5f6b3b3e9944866cd02aa8f90210e1de0
xcv58 <i@xcv58.com>
2020-01-28 17:35:27 -0800

    Update website title

--- gatsby/src/components/seo.js
+++ gatsby/src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title = "实时疫情信息聚合" }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`%s`}
       meta={[
         {
           name: `description`,
