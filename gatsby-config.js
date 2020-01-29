c7ac91e985805144e6ee7dd8b64a5e822d8ebaf6
xcv58 <i@xcv58.com>
2020-01-28 20:10:15 -0800

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,13 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA_TRACKING_ID,
+        head: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
