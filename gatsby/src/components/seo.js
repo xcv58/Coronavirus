880c0dee50d6a8f2936e7bc3719297ac501b3664
xcv58 <i@xcv58.com>
2020-01-28 17:34:24 -0800

    Format

--- gatsby/src/components/seo.js
+++ gatsby/src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title = '实时疫情信息聚合' }) {
+function SEO({ description, lang, meta, title = "实时疫情信息聚合" }) {
   const { site } = useStaticQuery(
     graphql`
       query {
