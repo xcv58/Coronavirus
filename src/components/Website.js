c473d9eda93b9d1ee4cd74d834a8d422c25719a1
xcv58 <i@xcv58.com>
2020-02-01 16:38:21 -0800

    Move reload button to the center

--- src/components/Website.js
+++ src/components/Website.js
@@ -1,9 +1,9 @@
-import React, { useContext, useRef, useState } from "react"
+import React, { useContext, useState } from "react"
 import Iframe from "react-iframe"
 import "./styles.css"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 import { LangContext, EN } from "./LangContext"
-import { Divider, Button, Skeleton, Typography } from "antd"
+import { Divider, Button, Skeleton } from "antd"
 
 const TRANSLATE_URL_PREFIX =
   "https://translate.google.com/translate?hl=en&sl=zh-CN&tl=en&u="
@@ -27,7 +27,6 @@ export default props => {
   const { name, className = "website" } = props
   const lang = useContext(LangContext)
   const url = getRenderUrl({ ...props, lang })
-  const iframeEl = useRef(null)
   const [loading, updateLoading] = useState(false)
   if (!url) {
     return null
@@ -38,25 +37,23 @@ export default props => {
   const skeleton = <Skeleton active className="iframe" />
   return (
     <div className={className}>
-      <div className="title">
-        <Button
-          icon="reload"
-          size="large"
-          loading={loading}
-          disabled={loading}
-          onClick={() => {
-            setTimeout(() => updateLoading(false), 640)
-            updateLoading(true)
-          }}
-        />
-        <div className="link">
-          <Divider>
-            <OutboundLink href={url} target="_blank" rel="noopener noreferrer">
-              {name}
-            </OutboundLink>
-          </Divider>
+      <Divider>
+        <div className="title">
+          <OutboundLink href={url} target="_blank" rel="noopener noreferrer">
+            {name}
+          </OutboundLink>
+          <Divider type='vertical' />
+          <Button
+            icon="reload"
+            loading={loading}
+            disabled={loading}
+            onClick={() => {
+              setTimeout(() => updateLoading(false), 640)
+              updateLoading(true)
+            }}
+          />
         </div>
-      </div>
+      </Divider>
       {loading ? skeleton : website}
       <hr />
     </div>
