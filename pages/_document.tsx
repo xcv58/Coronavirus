7bd1e9d3345be201617e726db2e03fd3576f0bfd
xcv58 <i@xcv58.com>
2020-02-15 15:20:57 -0800

    Fix the html dir

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document"
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" dir="rtl">
+      <Html lang="en" dir="ltr">
         <Head />
         <body>
           <Main />
