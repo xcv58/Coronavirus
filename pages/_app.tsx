8768308ccc7bd80444bb0f5dbc6e7ed4d921c65c
xcv58 <i@xcv58.com>
2020-02-15 11:44:13 -0800

    Add english title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import Head from "next/head"
 import { AppProps } from "next/app"
 import "antd/dist/antd.css"
 import "../components/styles.css"
+import useLang, { EN } from "../components/useLang"
 
 export default ({ Component, pageProps }: AppProps) => {
-  const title = "实时疫情信息聚合"
+  const lang = useLang()
+  const title = lang === EN ? 'Coronavirus realtime website' : "实时疫情信息聚合"
   return (
     <>
       <Head>
