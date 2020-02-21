93c2ae6484251a5eea0c27fb55163f357334235c
xcv58 <i@xcv58.com>
2020-02-20 20:51:40 -0800

    Add yarn analyze command

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   env: {
     GA_TRACKING_ID: process.env.GA_TRACKING_ID,
   },
-}
+})
